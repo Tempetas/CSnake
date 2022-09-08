d6e68b9715b257be86030efa92a4d2ecae8f2f3b
Tempetas <tampetus@gmail.com>
2022-09-08 17:31:26 +0200

    Minor improvements and fixes

--- src/main.c
+++ src/main.c
@@ -3,17 +3,18 @@
 #include <fxcg/heap.h>
 #include <fxcg/rtc.h>
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
+//Size of individual sprites and snake tiles
 #define TILE_SIZE 16
 
+//Game speed
 #define TICK_RATE 20
 
 #define STATE_MENU 0
 #define STATE_GAME 1
 
+//Amount of spawning apples
 #define MAX_FOOD 3
 
 struct Segment {
@@ -23,24 +24,33 @@ struct Segment {
 	struct Segment *prev;
 };
 
+//Snakes head
 struct Segment snake;
-struct Segment *food;
-
-int snakeLength = 0;
+//Ptr to snakes tail
 struct Segment *lastSegment;
 
-//right down left up
+//Snakes moving direction
 int direction = 1;
 
-//Screw good code standarts
+//Array of food locations
+struct Segment *food;
+
+int score = 0;
+
 int state = STATE_MENU;
 int lastTick;
 
 int MAX_X, MAX_Y;
 
-void addSegment() {
-	snakeLength++;
+//Random number generator
+int lastRand = 0;
 
+int randint() {
+	lastRand = 0x41C64E6D * lastRand + 0x3039;
+	return lastRand;
+}
+
+void addSegment() {
 	struct Segment* seg = sys_malloc(sizeof(struct Segment));
 
 	seg->x = lastSegment->x;
@@ -54,6 +64,8 @@ void addSegment() {
 	i->prev = seg;
 
 	lastSegment = seg;
+
+	score++;
 }
 
 void drawSquare(struct Segment* pos, int size, color_t color) {
@@ -90,20 +102,24 @@ bool input() {
 			if (state != STATE_GAME) {
 				state = STATE_GAME;
 
+				//Hide the hud
+				EnableStatusArea(3);
+
+				lastRand = RTC_GetTicks();
+
 				int startPos = TILE_SIZE * 4;
 
 				snake.x = startPos;
 				snake.y = startPos;
 
 				lastSegment = sys_malloc(sizeof(struct Segment));
-
 				lastSegment->prev = lastSegment;
 
 				food = sys_malloc(sizeof(struct Segment) * MAX_FOOD);
 
-				for (int i = 0, ticks = RTC_GetTicks(); i < MAX_FOOD; i++, ticks += 23) {
-					food[i].x = ((ticks % MAX_X) + 1) * TILE_SIZE;
-					food[i].y = ((ticks % MAX_Y) + 1) * TILE_SIZE;
+				for (int i = 0; i < MAX_FOOD; i++) {
+					food[i].x = ((randint() % (MAX_X - 3)) + 1) * TILE_SIZE;
+					food[i].y = ((randint() % (MAX_Y - 3)) + 1) * TILE_SIZE;
 				}
 			}
 		} else if (state == STATE_GAME) {
@@ -124,16 +140,22 @@ bool input() {
 					direction = 3;
 				break;
 			}
+		}
 
-			if ((column == 0x04) && (row == 0x09)) {
-				Bdisp_AllClr_VRAM();
+		if ((column == 0x04) && (row == 0x09)) {
+			return true;
+		}
+	}
 
-				state = STATE_MENU;
+	return false;
+}
 
-				sys_free(food);
-
-				return true;
-			}
+//Returns whether snakes head hit the body
+bool collidesWithSelf() {
+	//Works because the snake can only collide with itself if its length is above 3
+	for (struct Segment *seg = lastSegment; seg->prev != lastSegment; seg = seg->prev) {
+			if ((snake.x == seg->x) && (snake.y == seg->y)) {
+			return true;
 		}
 	}
 
@@ -162,29 +184,22 @@ void checkForFood() {
 	if (foodIndex) {
 		addSegment();
 
-		int ticks = RTC_GetTicks();
-
 		do {
-			food[foodIndex].x = ((ticks % MAX_X) + 1) * TILE_SIZE;
-			food[foodIndex].y = ((ticks % MAX_Y) + 1) * TILE_SIZE;
-
-			//Random magic const
-			ticks += 23;
+			food[foodIndex].x = ((randint() % MAX_X) + 1) * TILE_SIZE;
+			food[foodIndex].y = ((randint() % MAX_Y) + 1) * TILE_SIZE;
 		} while (collidesWithFood(false));
 	}
 }
 
 int main() {
-	lastTick = RTC_GetTicks() - 40;
+	lastTick = RTC_GetTicks() - TICK_RATE * 2;
 
-	MAX_X = LCD_WIDTH_PX / TILE_SIZE;
-	MAX_Y = LCD_HEIGHT_PX / TILE_SIZE;
+	MAX_X = LCD_WIDTH_PX / TILE_SIZE - 1;
+	MAX_Y = LCD_HEIGHT_PX / TILE_SIZE - 1;
 
 	while (true) {
-		if ((RTC_GetTicks() - lastTick) > 20) {
+		if ((RTC_GetTicks() - lastTick) > TICK_RATE) {
 			Bdisp_AllClr_VRAM();
-
-			DisplayStatusArea();
 
 			if (state == STATE_MENU) {
 				PrintXY(1, 1, "--Snake:", TEXT_MODE_NORMAL, COLOR_BLACK);
@@ -199,7 +214,7 @@ int main() {
 				if (direction % 2 == 0) {
 					snake.x -= (direction - 1) * TILE_SIZE;
 
-					if (snake.x > MAX_X * TILE_SIZE) {
+					if (snake.x > (MAX_X - 1) * TILE_SIZE) {
 						snake.x = TILE_SIZE;
 					} else if (snake.x < TILE_SIZE) {
 						snake.x = MAX_X * TILE_SIZE;
@@ -223,8 +238,24 @@ int main() {
 			lastTick = RTC_GetTicks();
 		}
 
-		if (input() && state == STATE_MENU) {
-			break;
+		//Should exit?
+		if (input() || (state == STATE_GAME && collidesWithSelf())) {
+			if (state == STATE_MENU) {
+				if ((RTC_GetTicks() - lastTick) > TICK_RATE * 3) {
+					break;
+				}
+			} else {
+				Bdisp_AllClr_VRAM();
+
+				state = STATE_MENU;
+
+				for (struct Segment* i = lastSegment; i->prev != lastSegment; i = i->prev) {
+					//TODO: does this work?
+					sys_free(i);
+				}
+
+				sys_free(food);
+			}
 		}
 	}
 
