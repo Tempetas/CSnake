37003ef0e0c2b4a0fe16b7676b08a9066446cc2c
Tempetas <tampetus@gmail.com>
2022-09-10 12:27:27 +0200

    Add util file

---
+++ src/itoa.h
@@ -0,0 +1,53 @@
+//Stolen from the internet
+
+void swap(char *x, char *y) {
+    char t = *x; *x = *y; *y = t;
+}
+
+char* reverse(char *buffer, int i, int j) {
+    while (i < j) {
+        swap(&buffer[i++], &buffer[j--]);
+    }
+
+    return buffer;
+}
+
+char* itoa(int value, char* buffer, int offset) {
+    int n = value;
+
+    int i = offset;
+    while (n) {
+        int r = n % 10;
+
+        if (r >= 10) {
+            buffer[i++] = 65 + (r - 10);
+        } else {
+            buffer[i++] = 48 + r;
+        }
+
+        n = n / 10;
+    }
+
+    if (i == offset) {
+        buffer[i++] = '0';
+    }
+
+    if (value < 0) {
+        buffer[i++] = '-';
+    }
+
+    buffer[i] = '\0';
+
+    return reverse(buffer, offset, i - 1);
+}
+
+int atoi(char* str) {
+    int res = 0;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        res = res * 10 + str[i] - '0';
+    }
+
+    return res;
+}
+
